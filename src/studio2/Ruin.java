bf1bc8a4e0779d99a48c15d6677b02cd3d1f2ae6
ryans <ryans@Aeon22.dhcp.wustl.edu>
2024-09-11 14:20:56 -0500

    Studio2 code

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,10 +1,117 @@
+
 package studio2;
+
+
+
+
+import java.util.Scanner;
+
+
+
 
 public class Ruin {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+
+
+public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("What is your Starting Amount");
+
+double startAmount = in.nextDouble();
+
+System.out.println("What is your Win Chance");
+
+double winChance = in.nextDouble();
+
+System.out.println("What is your Win Limit");
+
+double winLimit = in.nextDouble();
+
+System.out.println("How Many Days");
+
+int totalSimulations = in.nextInt();
+
+
+int dayNumber = 0;
+
+int amountWins = 0;
+
+int amountLoss = 0;
+
+
+for (int i = 0; i < totalSimulations; i++) {
+
+double currentScore = startAmount;
+
+
+boolean isGoing = true; 
+
+
+while (isGoing == true){
+
+if (Math.random() < winChance) {
+
+currentScore += 1;
 
 }
+
+else {
+
+currentScore -= 1;
+
+}
+
+
+if (currentScore == 0){
+
+System.out.println(currentScore+": LOSE");
+
+isGoing = false;
+
+amountLoss ++;
+
+}
+
+else if (currentScore >= winLimit){
+
+System.out.println(currentScore+": Win");
+
+amountWins ++;
+
+isGoing = false;
+
+}
+
+
+
+
+
+}
+
+dayNumber ++;
+
+System.out.println("Day "+ dayNumber);
+
+
+
+
+
+}
+
+System.out.println("Total Wins: "+amountWins + " Total Loss: "+ amountLoss);
+
+
+}
+
+}
+
+
+
+
+
+
+
+
